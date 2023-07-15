08761093846144182c4dbae6cbc81264b60be2e6
Bohdan <bohdan_heniuk@epam.com>
2023-07-15 14:23:20 +0300

    test ci

--- src/factorial/factorial.controller.ts
+++ src/factorial/factorial.controller.ts
@@ -7,7 +7,7 @@ export class FactorialController {
   constructor(private readonly factorialService: FactorialService) {}
 
   @Get('/:number')
-  getHello(@Param() params: any): Observable<number> {
+  getHello(@Param() params): Observable<number> {
     const { number } = params;
     return this.factorialService.calcFactorial(Number(number));
   }
