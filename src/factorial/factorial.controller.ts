1703760323c81ad1312b8a1db2feb87130811730
Bohdan <bohdan_heniuk@epam.com>
2023-07-16 21:41:13 +0300

    commit

--- src/factorial/factorial.controller.ts
+++ src/factorial/factorial.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { FactorialService } from './factorial.service';
 import { Observable } from 'rxjs';
+import { ApiTags, ApiParam } from '@nestjs/swagger';
 
+@ApiTags('Factorial')
 @Controller('/factorial')
 export class FactorialController {
   constructor(private readonly factorialService: FactorialService) {}
 
+  @ApiParam({ name: 'number', type: Number })
   @Get('/:number')
   getHello(@Param() params): Observable<number> {
     const { number } = params;
