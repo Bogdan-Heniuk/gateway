153828631c02f123347158d1e6a7ba00473fa178
Bohdan <bohdan_heniuk@epam.com>
2023-07-26 01:47:52 +0300

    commit36

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('API Example')
+    .setTitle('API Example34')
     .setVersion('1.0')
     .build();
 
